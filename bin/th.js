b52a674aea5a87b2cc6873df5ff9c79111f84988
luzhichun <luzhichun@corp.netease.com>
2021-04-13 21:35:03 +0800

    '项目初始化'

--- bin/th.js
+++ bin/th.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-const { program } = require("commander");
-const fs = require("fs");
-program.version(require("../package.json").version, "-v, --version");
+const { program } = require('commander')
+const fs = require('fs')
+program.version(require('../package.json').version, '-v, --version')
 program
-  .command("creat <name>")
-  .description("creat project")
-  .action((name) => require("../lib/init")(name));
-program.parse(process.argv);
+  .command('create <name>')
+  .description('create project')
+  .action(name => require('../lib/crm')(name))
+program.parse(process.argv)
